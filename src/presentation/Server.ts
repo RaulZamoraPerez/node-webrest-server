e2fed2e691660a4f38e57e9247a6b791b7f43180
Raul zamora <raulzamoraperez440@gmail.com>
2025-02-15 19:22:15 -0600

    rest

--- src/presentation/Server.ts
+++ src/presentation/Server.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options{
     port: number
+    routes: Router
     public_path?: string;
 }
 
@@ -11,22 +12,32 @@ export class Server{
     private app = express();
     private readonly port: number;
     private public_path: string;
+    private readonly routes: Router;
 
     constructor(options: Options){
-        const {port, public_path = "public"} = options;
+        //inicializar 
+        const {port, routes, public_path = "public"} = options;
         this.port = port;
         this.public_path = public_path;
+        this.routes = routes;
     }
 
     async start(){
 
 
         //* Middlewares
-
+        this.app.use(express.json());//cualquier peticion que pase al serever se va a parsear a json pq sino las peticiones no se van a poder leer  deja hacerlo con raw
+        this.app.use(express.urlencoded({extended:true}));// www-form-urlencoded
+        
         //* Public folder
         this.app.use(express.static(this.public_path)); //public folder
 
 
+        //* Routes
+       
+        this.app.use( this.routes);
+
+        //SPA
         this.app.get('*', (req, res)=>{
             const indexPath = path.join(__dirname, `../../${this.public_path}/index.html`);
             res.sendFile(indexPath);
@@ -37,3 +48,5 @@ export class Server{
         }); 
     }
 }
+
+//readonly es para q no se pueda modificar el valor de la propiedad
